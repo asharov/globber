98b86893e86bc492ec362bb77aea76cd0e250167
Jaakko Kangasharju <ashar@iki.fi>
2019-12-02 15:27:46 +0100

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as file:
 
 setuptools.setup(
     name='globber',
-    version='0.2.0',
+    version='0.2.1',
     author='Jaakko Kangasharju',
     author_email='ashar@iki.fi',
     description='Library for string matching with glob patterns',
